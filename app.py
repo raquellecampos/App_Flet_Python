3b81359d6acd579f2b639110e3a62f2962812111
Raquelle M. Campos <raquellecampos12@gmail.com>
2024-03-03 14:25:02 -0400

    Update app.py
    adicionando comentário na função

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flet import *
 import sqlite3
 
+# no terminal digitar: flet - r e o nome do arquivo para rodar a aplicação nesse caso app.py
 def main(page:Page):
     page.add(
         Text('Olá mundo')
